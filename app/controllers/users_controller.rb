de20363e56e01aafe04b8e06486f13efeeff0c6e
Kishor Bhat <kishorbhat@gmail.com>
2014-07-07 22:53:01 +0530

    updated delete, destroy methods, added params permit, created delete view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,22 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  def delete
+  	@user = User.find(params[:id])
+  end
+
+  def destroy
+  	user = User.find(params[:id])
+    user.destroy
+    flash[:notice] = "User '#{user.name}' destroyed successfully!"
+    redirect_to(:action => 'index')
+  end
+
+  private
+
+  def admin_user_param
+    params.require(:subject).permit(:first_name, :last_name, :email, :username, :password_digest)
+  end
+
 end
