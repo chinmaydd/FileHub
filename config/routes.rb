f6a60d637de1d1f1bf6bd2e7730b34fcd0fa1049
Kishor Bhat <kishorbhat@gmail.com>
2014-07-09 01:31:38 +0530

    changed default route to /uploads/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  
-  root "uploads#index"
+  root "uploads#home"
   match ':controller(/:action(/:id))', :via => [:get, :post]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
