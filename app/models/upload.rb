84f452a790114a2693e110a7c6982bd9f60e963a
Kishor Bhat <kishorbhat@gmail.com>
2014-07-08 22:17:07 +0530

    added active relations b/w user, uploads

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -3,5 +3,7 @@ class Upload < ActiveRecord::Base
 	has_attached_file :doc
 	validates_attachment_size :doc, :less_than => 10.megabytes
 
+	belongs_to :user
+
 	scope :sorted, lambda { order("uploads.position ASC")}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+
+	has_many :uploads
+
 end
