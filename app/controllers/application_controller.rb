01a8ab5cbd043bff873e55d706cf66cad022a9a3
shiva <shiva96b@gmail.com>
2014-07-08 23:26:04 +0530

    edited application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+private
+
+  def confirm_logged_in
+    unless session[:user_id]
+      flash[:notice] = "Please log in."
+      redirect_to(:controller => 'uploads', :action => 'home')
+      return false # halts the before_action
+    else
+      return true
+    end
+  end
+
 end
